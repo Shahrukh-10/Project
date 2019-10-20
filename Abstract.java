b9c931b424e0dd1d6af6d330bb6dc71afe619215
Shahrukh-10 <56799965+Shahrukh-10@users.noreply.github.com>
2019-10-20 23:34:34 +0530

    Create  object of a function in class

---
+++ Abstract.java
@@ -0,0 +1,16 @@
+class Abstract
+{
+	void  btech()
+	{
+		System.out.println("rsrtsrs");
+	}
+}
+class Colee extends Abstract
+{
+	public static void main(String[] args)
+	{
+		Colee obj= new Colee();
+		obj.btech();
+	}
+
+}
